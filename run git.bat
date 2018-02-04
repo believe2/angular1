1f7aeb175f897bf93d105a9ff450152edcbd56f4
believe2 <johnbelieve2@gmail.com>
2018-02-04 11:46:04 +0800

    first commit

--- run git.bat
+++
@@ -1,9 +0,0 @@
-git config --global user.name "believe2"
-git config --global user.email "johnbelieve2@gmail.com"
-git init
-git add -A
-git commit -m "first commit"
-git remote set-url origin https://believe2:idbehold2@github.com/believe2/angular1.git
-git push -u origin master
-
-pause
